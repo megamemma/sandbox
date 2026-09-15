3d669815d7f81ebf39534e453a881ce35c0ae686
Emma K. <sireneviyy@gmail.com>
2026-09-15 23:44:23 +0500

    Add on click func for city search

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,18 +1,26 @@
 const apiKey = "76365358b2a5cd215d7ce03243621be7";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appId=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appId=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".degrees").innerHTML = data.main.temp + "°c";
+    document.querySelector(".degrees").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
 }
+
+searchBtn.addEventListener("click", ()=> {
+    checkWeather();
+})
 
 checkWeather();
 
